9abb29fa90e162be844e6ffbe38ee7374da46c4f
rinku <rinkasai226@gmail.com>
2018-05-07 00:20:43 +0530

    saved one

---
+++ publish_git2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(publish_git2.Startup))]
+namespace publish_git2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ publish_git2/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="publish_git2.MvcApplication" Language="C#" %>
